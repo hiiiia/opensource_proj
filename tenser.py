8b74cc146c66499151d03b6e69501fe38f491d01
hiiia <gksqlc0406@mgmail.com>
2022-11-27 19:45:39 +0900

    데이터 추출

--- tenser.py
+++ tenser.py
@@ -24,17 +24,38 @@
 #     json.dump(exercise_dict, f)
 
 
+# import json
+# import numpy as np
+# import tensorflow as tf
+#
+# def get_bbox(str):
+#
+#     obj = json.loads(str.decode('utf-8'))
+#     bbox = obj['bounding_box']
+#     return np.array([bbox['x'], bbox['y'], bbox['height'], bbox['width']], dtype='f')
+#
+# def get_multiple_bboxes(str):
+#     return [[get_bbox(x) for x in str]]
+#
+# raw = tf.placeholder(tf.string, [None])
+# [parsed] = tf.py_func(get_multiple_bboxes, [raw], [tf.float32])
+
 import json
-import numpy as np
-import tensorflow as tf
 
-def get_bbox(str):
-    obj = json.loads(str.decode('utf-8'))
-    bbox = obj['bounding_box']
-    return np.array([bbox['x'], bbox['y'], bbox['height'], bbox['width']], dtype='f')
+file_path = "./data/D08-1-033.json"
 
-def get_multiple_bboxes(str):
-    return [[get_bbox(x) for x in str]]
+with open(file_path, 'r', encoding='UTF-8') as file:
+    data = json.load(file)
+    exercies_type = data['type_info']['exercise']
+    exercies_data_v1= data['frames'][0]
+    print(exercies_type)
+    for exercies_type in data['frames']:
+        for exercies_data_view,data in exercies_type.items():
+            for x,point_data in data['pts'].items():
+                print(f'{x}',end=' : ')
+                for t,y in point_data.items():
+                    print(f'{t} {y}',end=' ')
+                print("")
 
-raw = tf.placeholder(tf.string, [None])
-[parsed] = tf.py_func(get_multiple_bboxes, [raw], [tf.float32])
+            print(data['active'])
+
