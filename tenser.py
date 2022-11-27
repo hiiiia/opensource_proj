424d2b193dd8630cb1effcee643246482e39a329
hiiia <gksqlc0406@mgmail.com>
2022-11-27 21:52:17 +0900

    데이터 추출/to csv

--- tenser.py
+++ tenser.py
@@ -41,21 +41,66 @@
 # [parsed] = tf.py_func(get_multiple_bboxes, [raw], [tf.float32])
 
 import json
+import pandas as pd
+import os
 
-file_path = "./data/D08-1-033.json"
+global body_cord
 
-with open(file_path, 'r', encoding='UTF-8') as file:
-    data = json.load(file)
-    exercies_type = data['type_info']['exercise']
-    exercies_data_v1= data['frames'][0]
-    print(exercies_type)
-    for exercies_type in data['frames']:
-        for exercies_data_view,data in exercies_type.items():
-            for x,point_data in data['pts'].items():
-                print(f'{x}',end=' : ')
-                for t,y in point_data.items():
-                    print(f'{t} {y}',end=' ')
-                print("")
+file_path = "./data"
+file_list = os.listdir(file_path)
+file_list = [file_json for file_json in file_list if file_json.endswith(".json")]
+print(file_list)
+for file_json in file_list:
+    with open(file_path+'/'+file_json, 'r', encoding='UTF-8') as file:
+        data = json.load(file)
+        exercies_type = data['type_info']['exercise']
+        print(exercies_type)
 
-            print(data['active'])
+        file = "./" + exercies_type + ".csv"
+        if not os.path.exists(file):
+            body_cord = pd.DataFrame(columns=[
 
+                'Nose_x','Nose_y',\
+                'Left Eye_x', 'Left Eye_y',\
+                'Right Eye_x','Right Eye_y',\
+                'Left Ear_x','Left Ear_y',\
+                'Right Ear_x','Right Ear_y',\
+                'Left Shoulder_x','Left Shoulder_y',\
+                'Right Shoulder_x','Right Shoulder_y',\
+                'Left Elbow_x','Left Elbow_y',\
+                'Right Elbow_x','Right Elbow_y',\
+                'Left Wrist_x','Left Wrist_y',\
+                'Right Wrist_x','Right Wrist_y',\
+                'Left Hip_x','Left Hip_y',\
+                'Right Hip_x','Right Hip_y',\
+                'Left Knee_x','Left Knee_y',\
+                'Right Knee_x','Right Knee_y',\
+                'Left Ankle_x','Left Ankle_y',\
+                'Right Ankle_x','Right Ankle_y',\
+                'Neck_x','Neck_y',\
+                'Left Palm_x','Left Palm_y',\
+                'Right Palm_x','Right Palm_y',\
+                'Back_x','Back_y',\
+                'Waist_x','Waist_y',\
+                'Left Foot_x','Left Foot_y',\
+                'Right Foot_x','Right Foot_y',\
+                'active'], dtype=float)
+        else:
+            body_cord = pd.read_csv(file, sep=",")
+
+        for exercies_type in data['frames']:
+            for exercies_data_view,data in exercies_type.items():
+                tmp_list = []
+
+                for x,point_data in data['pts'].items():
+                    print(f'{x}',end=' : ')
+                    for t,y in point_data.items():
+                        print(f'{t} {y}',end=' ')
+                        tmp_list.append(y)
+                    print("")
+
+
+                print(data['active'])
+                tmp_list.append(data['active'])
+                body_cord = body_cord.append(pd.Series(tmp_list,index=body_cord.columns), ignore_index=True)
+        body_cord.to_csv(file,index=False)
